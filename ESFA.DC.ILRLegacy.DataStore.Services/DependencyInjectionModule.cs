02a20dfe9e49b11f51f760950e737d300de5c133
jamie <jamie.coales@education.gov.uk>
2018-11-23 13:36:01 +0000

    move di to service project or nuget doesnt pick it up

---
+++ ESFA.DC.ILRLegacy.DataStore.Services/DependencyInjectionModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using ESFA.DC.ILRLegacy.DataStore.DataAccessLayer.Mappers;
+using ESFA.DC.ILRLegacy.DataStore.DataAccessLayer.Repositories;
+using ESFA.DC.ILRLegacy.DataStore.Interfaces;
+
+namespace ESFA.DC.ILRLegacy.DataStore.Services
+{
+    public class DependencyInjectionModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<IFM70Repository>().As<FM70Repository>();
+            builder.RegisterType<IFM70Mapper>().As<FM70Mapper>();
+
+            builder.RegisterType<IFM70DataService>().As<FM70DataService>();
+        }
+    }
+}
